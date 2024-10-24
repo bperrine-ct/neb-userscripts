4539c8fff5fc0debef31eda371047123cc6c75e7
Benedict Perrine <bperrine@chirotouch.com>
2024-10-24 14:16:20 -0700

    Update bold-bracket-text.user.js

--- bold-bracket-text.user.js
+++ bold-bracket-text.user.js
@@ -5,6 +5,7 @@
 // @description  Bold text inside brackets and "Age: x" where x is any number without altering existing styles
 // @author       
 // @match        https://chirotouch.atlassian.net/*
+// @icon         https://i.postimg.cc/MGG4FB49/image.png
 // @grant        none
 // ==/UserScript==
 
