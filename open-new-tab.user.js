94519b14cfc8a1d07f1c94d797c15be7cc42af81
Benedict Perrine <bperrine@chirotouch.com>
2024-10-24 12:00:09 -0700

    Update open-new-tab.user.js

--- open-new-tab.user.js
+++ open-new-tab.user.js
@@ -1,14 +1,14 @@
-  // ==UserScript==
-  // @name         Open New Tab
-  // @namespace    http://tampermonkey.net/
-  // @version      0.1
-  // @description  Open a new tab when clicking on a link
-  // @author       You
-  // @match        https://chirotouch.atlassian.net/*
-  // @grant        none
-  // ==/UserScript==
+// ==UserScript==
+// @name         Open New Tab
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  Open a new tab when clicking on a link and prevent default behavior
+// @author       You
+// @match        https://chirotouch.atlassian.net/*
+// @grant        none
+// ==/UserScript==
 
-  (function() {
+(function() {
     'use strict';
 
     function openLinksInNewTab() {
@@ -16,6 +16,7 @@
         linkButtons.forEach(button => {
             button.addEventListener('click', function(event) {
                 event.preventDefault();
+                event.stopPropagation();  // Stop the event from propagating further
                 const link = button.querySelector('a');
                 if (link) {
                     window.open(link.href, '_blank');
@@ -27,7 +28,7 @@
     // Run the function on page load
     window.addEventListener('load', openLinksInNewTab);
 
-    // Set up a MutationObserver to reapply the changes
+    // Set up a MutationObserver to reapply the changes when DOM changes
     const observer = new MutationObserver(openLinksInNewTab);
     observer.observe(document.body, { childList: true, subtree: true });
 })();
