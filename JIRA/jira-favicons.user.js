2efbfe9aa03b2e5123a38bacf0181e644db3c698
Benedict Perrine <bperrine@chirotouch.com>
2025-02-20 10:15:42 -0800

    Update jira-favicons.user.js

--- JIRA/jira-favicons.user.js
+++ JIRA/jira-favicons.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         JIRA Favicon Changer
+// @name         JIRA Favicon and Title Cleaner
 // @namespace    http://tampermonkey.net/
-// @version      1.4
-// @description  Change favicon based on issue type for chirotouch.atlassian.net
+// @version      2.0
+// @description  Changes favicon based on issue type and cleans up page titles
 // @match        https://chirotouch.atlassian.net/*
 // @grant        none
 // @icon         https://chirotouch.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10315?size=medium
@@ -13,8 +13,25 @@
 (function () {
 	'use strict';
 
+	let previousTitle = document.title;
+
 	const defaultFavicon =
 		'https://chirotouch.atlassian.net/rest/api/2/universal_avatar/view/type/project/avatar/14627?size=xxlarge';
+
+	// Title cleaning functions
+	const cleaners = [
+		// Remove sprint prefix (e.g., "GottaKeepEmAllocated-Sp-4-25 - ")
+		title => title.replace(/^[^-]+-Sp-\d+-\d+\s+-\s+/, ''),
+		// Remove anything in brackets (e.g., "[Done]" or "[In Progress]")
+		title => title.replace(/\s*\[[^\]]*\]\s*/g, ' '),
+		// Clean up any double spaces and trim
+		title => title.replace(/\s+/g, ' ').trim(),
+	];
+
+	// Apply all cleaners in sequence
+	const cleanTitle = title => {
+		return cleaners.reduce((current, cleaner) => cleaner(current), title);
+	};
 
 	function isIssuePage() {
 		return /\/browse\/[A-Z]+-\d+/.test(window.location.pathname);
@@ -48,13 +65,41 @@
 		document.head.appendChild(linkElement);
 	}
 
+	// Handle both favicon and title changes
+	function updatePage() {
+		// Update favicon
+		changeFavicon();
+
+		// Update title
+		const currentTitle = document.title;
+		if (currentTitle !== previousTitle) {
+			console.log('Title changed from:', previousTitle, 'to:', currentTitle);
+
+			const newTitle = cleanTitle(currentTitle);
+			if (newTitle !== currentTitle) {
+				document.title = newTitle;
+				console.log('Cleaned title to:', newTitle);
+			}
+
+			previousTitle = document.title;
+		}
+	}
+
 	// Initial check
-	changeFavicon();
+	updatePage();
 
 	// Set up a MutationObserver to watch for changes in the DOM
-	const observer = new MutationObserver(changeFavicon);
+	const observer = new MutationObserver(updatePage);
 	observer.observe(document.body, { childList: true, subtree: true });
 
+	// Also observe title changes specifically
+	const titleObserver = new MutationObserver(updatePage);
+	titleObserver.observe(document.querySelector('title') || document.head, {
+		subtree: true,
+		characterData: true,
+		childList: true,
+	});
+
 	// Listen for URL changes
-	window.addEventListener('popstate', changeFavicon);
+	window.addEventListener('popstate', updatePage);
 })();
